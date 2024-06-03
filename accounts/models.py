cbbb710b0c1c96932b7df26723b32c4d870ac193
npoudelp <npoudelp@gmail.com>
2024-06-03 19:06:47 +0545

    suggestion provided. acknoledge this

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+# why made this database when you have nothing new field to add????!!!! @batti tauke... why not use django user model???
 class Login(models.Model):
     user_name = models.CharField(max_length=20)
     hash_password = models.TextField()
